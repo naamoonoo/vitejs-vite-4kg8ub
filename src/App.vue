5498281eeecfdf0af8a438ef90af5096749686f9
naamoonoo <naamoonoo.y@gmail.com>
2024-09-07 09:00:09 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">Todo List</h1>
+    <div class="mb-4">
+      <input
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+        type="text"
+        placeholder="새로운 할 일을 입력하세요"
+        class="w-full p-2 border rounded"
+      />
+    </div>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        class="flex items-center justify-between p-2 border-b"
+      >
+        <div class="flex items-center flex-grow mr-4">
+          <input
+            type="checkbox"
+            v-model="todo.completed"
+            class="mr-2"
+          />
+          <input
+            v-if="todo.isEditing"
+            v-model="todo.text"
+            @blur="finishEdit(todo)"
+            @keyup.enter="finishEdit(todo)"
+            type="text"
+            class="flex-grow p-1 border rounded"
+          />
+          <span
+            v-else
+            @dblclick="startEdit(todo)"
+            :class="{ 'line-through': todo.completed }"
+          >
+            {{ todo.text }}
+          </span>
+        </div>
+        <div>
+          <button @click="removeTodo(todo.id)" class="text-red-500 mr-2">삭제</button>
+          <button @click="startEdit(todo)" class="text-blue-500" v-if="!todo.isEditing">수정</button>
+        </div>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+interface Todo {
+  id: number
+  text: string
+  completed: boolean
+  isEditing: boolean
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+const newTodo = ref('')
+const todos = ref<Todo[]>([])
+
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    todos.value.push({
+      id: Date.now(),
+      text: newTodo.value,
+      completed: false,
+      isEditing: false
+    })
+    newTodo.value = ''
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+const removeTodo = (id: number) => {
+  todos.value = todos.value.filter(todo => todo.id !== id)
 }
+
+const startEdit = (todo: Todo) => {
+  todo.isEditing = true
+}
+
+const finishEdit = (todo: Todo) => {
+  todo.isEditing = false
+  todo.text = todo.text.trim()
+  if (!todo.text) {
+    removeTodo(todo.id)
+  }
+}
+</script>
+
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+const model = ref('')
+
+</script>
+
+<template>
+    <div class="container">
+        <div c>
+        <input v-model="model" class="input"/>
+        <button>추가</button>
+        </div>
+    <div v-if="model.includes('x')" class="warning">사용할 수 없는 단어가 포함되어있어요.</div>
+</div>
+
+</template>
+
+<style scoped>
+.container {
+    width:100%;
+    padding: 0 8px;
+    display: flex;    
+    flex-direction: column
+}
+
+.input {
+    width:100%;
+}
+
+.warning {
+    color:red;
+    text-align: center;
+}
+</style>
